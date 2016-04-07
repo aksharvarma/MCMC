900c55b5d25e1c7118b3c49f97e42f0e84bd5df8
Parth Ganatra <ganatraparth1995@gmail.com>
2016-04-08 00:22:25 +0530

    debug and optimize code

--- mcmc.c
+++ mcmc.c
@@ -3,6 +3,8 @@
 #include<math.h>
 #include<time.h>
 #include<limits.h>
+#include<string.h>
+
 
 
 #define min(a,b) (((a)>(b))?b:a)
@@ -16,16 +18,16 @@ int dot(int *a, int *b, int n);
 void print_matrix(int **matrix, int rows, int cols);
 void print_vector(int *vector, int n);
 double score(int beta, int *x, int *g, int n);
-int *propose(int w_max, int *x, int *w, int n);
+int *propose(int w_max, int *x, int *w, int n,int *x_proposal);
 int *mcmc(int iterations, int w_max, int *w, int *g, int n, int beta_start, int beta_increment);
 int knapsack_dp(int w_max, int *w, int *g, int n);
   
 int main(){
   srand(time(NULL));
   
-  int i,n=rand()%30+1;
-  int mcmc_runs=10;
-  int iterations=pow(10,5);
+  int i,n=rand()%100+1;
+  int mcmc_runs=1000;
+  int iterations=pow(10,2);
   int *w=(int*)malloc(n*sizeof(int));
   int *g=(int*)malloc(n*sizeof(int));
   int w_max;
@@ -33,7 +35,7 @@ int main(){
   int *final_x=(int*)malloc(n*sizeof(int));
   int best_haul=-99, haul=0;
   
-  double beta_start=0.005, beta_increment=0.005;
+  double beta_start=0.0005, beta_increment=0.0005;
   
 
   w_max=(n*n)/10+rand()%(n/2);
@@ -65,7 +67,8 @@ int main(){
     /* print_vector(x,n); */
     haul=dot(x,g,n);
     if(haul>best_haul){
-      final_x=x;
+      //final_x=x;
+        memcpy(final_x,(const void*)x,sizeof(int)*n);      
       best_haul=haul;
     }
   }
@@ -73,13 +76,12 @@ int main(){
   printf("The final state is:\n");
   print_vector(final_x,n);
   printf("The haul is worth: %d\n\n", best_haul);
-
-
-
  
  deterministic:
+  int best_haul_det = knapsack_dp(w_max, w, g, n);
   printf("Deterministic: %d\n", knapsack_dp(w_max, w, g, n));
 
+  printf("Error: %d\n",(best_haul - best_haul_det)*100/best_haul_det);
   free(g);
   free(w);
   free(x);
@@ -124,17 +126,16 @@ int dot(int *a, int *b, int n){
 }
 
 double score(int beta, int *x, int *g, int n){
-  return exp(beta*dot(x,g,n));
+  return beta*dot(x,g,n);
 }
 
-
-int *propose(int w_max, int *x, int *w, int n){
+int *propose(int w_max, int *x, int *w, int n,int *x_proposal){
   int i;
   int random_index=rand()%n;
   /* printf("i:%d\n", random_index); */
   int* proposal=(int*)malloc(n*sizeof(int));
 
-  for(i=0;i<n;i++)
+  for(i=0;i<n;i++)      //Can be optimized
     if(i==random_index)
       proposal[i]=!x[i];
     else
@@ -143,35 +144,47 @@ int *propose(int w_max, int *x, int *w, int n){
   /* print_vector(proposal,n); */
   if(dot(w,proposal,n)<w_max){
     /* printf("I'm done here.\n"); */
-    return proposal;
+    
+    memcpy(x_proposal,(const void*)proposal,sizeof(int)*n);      
+    free(proposal);
+    return x_proposal;
   }
   else
-    return propose(w_max,x,w,n);
+  {
+    free(proposal);
+    return propose(w_max,x,w,n,x_proposal);
+  }
 }
 
 int *mcmc(int iterations, int w_max, int *w, int *g, int n, int beta_start, int beta_increment){
   int i;
   int beta=beta_start;
-  int *x_current=(int*)calloc(n,sizeof(int));
+  int *x_current=(int*)malloc(n*sizeof(int));
   int *x_proposed=(int*)malloc(n*sizeof(int));
+  int *x_proposal=(int*)malloc(n*sizeof(int));
   int *x_best=(int*)malloc(n*sizeof(int));
   double best_score=0;
   double current_score=0, proposed_score=0, accept_probability;
 
+  for(i=0;i<n;i++)
+      x_current[i]=0;
   for(i=0;i<iterations;i++){
-    x_proposed=propose(w_max, x_current, w, n);
+    x_proposed=propose(w_max, x_current, w, n,x_proposal);
 
     current_score=score(beta,x_current,g,n);
     proposed_score=score(beta,x_proposed,g,n);
-    accept_probability=min(1,proposed_score-current_score);
+    accept_probability=min(1,exp(proposed_score-current_score));
 
     if(proposed_score>=best_score){
-      x_best=x_proposed;
-      x_current=x_proposed;
+        memcpy(x_best,(const void*)x_proposed,sizeof(int)*n);      
+        //memcpy(x_current,(const void*)x_proposed,sizeof(int)*n);      
+      //x_best=x_proposed;
+      //x_current=x_proposed;
     }
 
     if(U<=accept_probability){
-      x_current=x_proposed;
+      //x_current=x_proposed;
+        memcpy(x_current,(const void*)x_proposed,sizeof(int)*n);      
     }
 
     if(i%(iterations/100)==0)
@@ -217,5 +230,4 @@ int knapsack_dp(int w_max, int *w, int *g, int n){
   /* print_matrix(knap, n+1, w_max+1); */
   return knap[n][w_max];
 }
-
 
