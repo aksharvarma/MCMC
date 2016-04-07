b87ce15e06f256b142e4c02b8e75ab5ff4f7bd8c
Akshar Varma <aksharvarma@gmail.com>
2016-04-07 15:13:36 +0530

    Added dynamic programming implementation.

--- mcmc.c
+++ mcmc.c
@@ -6,6 +6,7 @@
 
 
 #define min(a,b) (((a)>(b))?b:a)
+#define max(a,b) (((a)<(b))?b:a)
 
 /* This allows generationg of uniform random variables */
 #define U ((long double)rand()/RAND_MAX)
@@ -17,36 +18,48 @@ void print_vector(int *vector, int n);
 double score(int beta, int *x, int *g, int n);
 int *propose(int w_max, int *x, int *w, int n);
 int *mcmc(int iterations, int w_max, int *w, int *g, int n, int beta_start, int beta_increment);
-
+int knapsack_dp(int w_max, int *w, int *g, int n);
   
 int main(){
   srand(time(NULL));
-  int i,n=14;
-  int mcmc_runs=100;
-  int iterations=pow(10,5)/2;
+  
+  int i,n=rand()%30+1;
+  int mcmc_runs=10;
+  int iterations=pow(10,5);
   int *w=(int*)malloc(n*sizeof(int));
   int *g=(int*)malloc(n*sizeof(int));
-  int w_max=150;
-
+  int w_max;
   int *x=(int*)malloc(n*sizeof(int));
   int *final_x=(int*)malloc(n*sizeof(int));
   int best_haul=-99, haul=0;
   
   double beta_start=0.005, beta_increment=0.005;
   
+
+  w_max=(n*n)/10+rand()%(n/2);
+  /* scanf("%d", &w_max); */
   for(i=0;i<n;i++){
-    scanf("%d ", &w[i]);
+    w[i]=rand()%(n*5)+1;
+    /* scanf("%d ", &w[i]); */
   }
 
   for(i=0;i<n;i++){
-    scanf("%d ", &g[i]);
+    g[i]=rand()%(n*10)+rand()%n+1;
+    /* scanf("%d ", &g[i]); */
   }
 
+  printf("n: %d\nw_max: %d\n", n, w_max);
+  printf("W:\n");
+  print_vector(w,n);
+  printf("G:\n");
+  print_vector(g,n);
+  printf("\n");
   /* for(i=0;i<n;i++){ */
   /*   printf("w: %d g: %d\n", w[i], g[i]); */
   /* } */
     
-
+  /* goto deterministic; */
+  
   for(i=0;i<mcmc_runs;i++){
     x=mcmc(iterations, w_max, w, g, n, beta_start, beta_increment);
     /* print_vector(x,n); */
@@ -59,21 +72,33 @@ int main(){
 
   printf("The final state is:\n");
   print_vector(final_x,n);
-  printf("The haul is worth: %d\n", best_haul);
-  
+  printf("The haul is worth: %d\n\n", best_haul);
+
+
+
+ 
+ deterministic:
+  printf("Deterministic: %d\n", knapsack_dp(w_max, w, g, n));
+
+  free(g);
+  free(w);
+  free(x);
+  free(final_x);  
   return 0;
 }
 
 
 void print_matrix(int **matrix, int rows, int cols){
   int i,j;
-
+  
+  printf("__________________\n");
   for(i=0;i<rows;i++){
     for(j=0;j<cols;j++){
       printf("%d ", matrix[i][j]);
     }
     printf("\n");
   }
+  printf("__________________\n");
   return;
 }
 
@@ -121,8 +146,7 @@ int *propose(int w_max, int *x, int *w, int n){
     return proposal;
   }
   else
-    propose(w_max,x,w,n);
-    
+    return propose(w_max,x,w,n);
 }
 
 int *mcmc(int iterations, int w_max, int *w, int *g, int n, int beta_start, int beta_increment){
@@ -141,18 +165,57 @@ int *mcmc(int iterations, int w_max, int *w, int *g, int n, int beta_start, int 
     proposed_score=score(beta,x_proposed,g,n);
     accept_probability=min(1,proposed_score-current_score);
 
-    if(proposed_score>best_score){
+    if(proposed_score>=best_score){
       x_best=x_proposed;
       x_current=x_proposed;
     }
 
-    if(U<accept_probability){
+    if(U<=accept_probability){
       x_current=x_proposed;
     }
 
     if(i%(iterations/100)==0)
       beta+=beta_increment;
   }
+
+  free(x_current);
+  free(x_proposed);
   
   return x_best;
 }
+
+
+int knapsack_dp(int w_max, int *w, int *g, int n){
+  /* printf("Hey\n"); */
+  int i, j;
+  int **knap=(int**)malloc((n+1)*sizeof(int*));
+  for(i=0;i<n+1;i++)
+    knap[i]=(int*)malloc((w_max+1)*sizeof(int));
+
+  /* print_vector(w, n); */
+  /* print_vector(g, n); */
+  /* printf("Yeah?\n"); */
+
+  for(j=0;j<=w_max;j++)
+    knap[0][j]=0;
+
+  // Build table K[][] in bottom up manner
+  for (i =1; i <= n; i++){
+    for (j = 0; j <= w_max; j++){
+      /* printf("In the bellows: %d %d\n", i, j); */
+      if(w[i-1]>j){
+	knap[i][j] = knap[i-1][j];
+      }
+      else{
+	knap[i][j] = max(g[i-1] + knap[i-1][j-w[i-1]],  knap[i-1][j]);
+      }
+    }
+  }
+
+  /* printf("Out of my misery\n"); */
+
+  /* print_matrix(knap, n+1, w_max+1); */
+  return knap[n][w_max];
+}
+
+
